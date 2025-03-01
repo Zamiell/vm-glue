e18c878a652bbfef8824e7dddd5d2a0cc06bfa88
Zamiell <5511220+Zamiell@users.noreply.github.com>
2025-03-01 14:53:39 +0000

    feat: listen on port 6969

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,49 @@
+import { $ } from "bun";
 import { Hono } from "hono";
+import { z } from "zod";
 
 const PORT = 6969;
 
 const app = new Hono();
 
-// eslint-disable-next-line @typescript-eslint/require-await
+const browserSchema = z.object({
+  url: z.string().url(),
+});
+
+app.get("/", (c) => c.text("server operational"));
+
 app.post("/browser", async (c) => {
-  c.status(200);
+  let body: unknown;
+  try {
+    body = await c.req.json();
+  } catch {
+    return c.json(
+      {
+        error: "validation failed",
+        details: "POST data must be JSON",
+      },
+      400,
+    );
+  }
+
+  const result = browserSchema.safeParse(body);
+  if (!result.success) {
+    return c.json(
+      {
+        error: "validation failed",
+        details: result.error.format(),
+      },
+      400,
+    );
+  }
+
+  const { url } = result.data;
+
+  // There is no need to wait for the command to complete before returning a response.
+  // eslint-disable-next-line @typescript-eslint/no-floating-promises
+  $`start chrome ${url}`;
+
+  return c.text("200", 200);
 });
 
 // eslint-disable-next-line import-x/no-default-export
--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@
 # https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-bun run "$DIR/src/main.ts"
+bun run --watch "$DIR/src/main.ts"
